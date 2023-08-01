44c3a8aec3cb46bbb397789a87f604ad26f695ea
ChemiatEdgar <chemiatedgar@gmail.com>
2023-08-01 13:55:09 +0300

    Initial commit

---
+++ app/src/main/java/com/esj/navigationproject/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.esj.navigationproject
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.esj.navigationproject.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+private lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.button.setOnClickListener {
+            if (!TextUtils.isEmpty(binding.editTextText.text.toString())){
+                val bundle: Bundle = bundleOf("user_input" to binding.editTextText.text.toString() )
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+            }else{
+                Toast.makeText(activity, "Please enter your name", Toast.LENGTH_LONG).show()
+            }
+
+        }
+        return binding.root
+    }
+
+
+}
